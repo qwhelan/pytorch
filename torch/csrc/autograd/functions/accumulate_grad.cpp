b7186daa620114e5063069a493e74cf8ef382562
pritam <pritam.damania@fb.com>
2020-02-12 20:34:20 -0800

    Unify gradient accumulation between distributed autograd and local autograd engine.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33214
    
    Distributed autograd had some custom logic in terms of how we
    accumulated gradients. This was mostly done early on to enable basic
    functionality. Although, in the long term we should merge this logic with what
    we have in the local autograd engine. A lot of work has gone into ensuring we
    accumulate grads correctly and efficiently and we should reuse that as a
    starting point.
    
    We can investigate if we need further custom logic for distributed autograd
    later on if we need additional optimizations.
    
    In this PR I've merged the gradient accumulation logic and also the gradient
    hooks. As a result, now gradient hooks are called in distributed autograd as
    well.
    ghstack-source-id: 98233775
    
    Differential Revision: [D19843284](https://our.internmc.facebook.com/intern/diff/D19843284/)

--- torch/csrc/autograd/functions/accumulate_grad.cpp
+++ torch/csrc/autograd/functions/accumulate_grad.cpp
@@ -22,55 +22,48 @@ AccumulateGrad::AccumulateGrad(Variable variable_)
   add_input_metadata(variable);
 }
 
-auto AccumulateGrad::apply(variable_list&& grads) -> variable_list {
-  // XXX: this method is not thread-safe!
-  check_input_variables("AccumulateGrad", grads, 1, 0);
-
-  if (!grads[0].defined())
-    return {};
-  if (variable.grad_fn())
-    throw std::logic_error("leaf variable has been moved into the graph interior");
-  if (!variable.requires_grad())
-    return {};
-
-  auto new_grad = std::move(grads[0]);
+void AccumulateGrad::accumulateGradAndCallHooks(
+    const Variable& variable,
+    at::Tensor variable_grad,
+    at::Tensor new_grad,
+    bool has_post_hooks,
+    std::function<void(at::Tensor&&)> update_grad_fn) {
   for (auto& hook : impl::hooks(variable)) {
     new_grad = (*hook)({new_grad})[0];
   }
 
-  at::Tensor& grad = variable.grad();
-  if (!grad.defined()) {
+  if (!variable_grad.defined()) {
     // under following condition, we can avoid clone()
-    if (!GradMode::is_enabled()
-        && !new_grad.is_sparse()
-        && new_grad.is_contiguous()
-        && new_grad.use_count() <= 1 + !post_hooks().empty()) {
+    if (!GradMode::is_enabled() && !new_grad.is_sparse() &&
+        new_grad.is_contiguous() &&
+        new_grad.use_count() <= 2 + has_post_hooks) {
       // first check it is in first-order grad only mode
       // then check not sparse before is_contiguous
       // then check contiguous, otherwise later in place accumulation may fail
       // and lastly, check it is the last reference before we grab it.
       // If the function has post hooks (for example, a DDP allreduce hook),
-      // call_function in Engine.cpp will temporarily bump the refcount by one, hence the
-      // addition of !post_hooks().empty().
-      variable.grad() = new_grad.detach();
+      // call_function in Engine.cpp will temporarily bump the refcount by one,
+      // hence the addition of has_post_hooks.
+      // We use 2 + has_post_hooks instead of 1 + has_post_hooks since calling
+      // this function bumps the ref count.
+      update_grad_fn(new_grad.detach());
     } else {
       if (new_grad.is_sparse()) {
-        variable.grad() = new_grad.clone();
+        update_grad_fn(new_grad.clone());
       } else {
-        variable.grad() = new_grad.clone(at::MemoryFormat::Contiguous);
+        update_grad_fn(new_grad.clone(at::MemoryFormat::Contiguous));
       }
     }
   } else if (!GradMode::is_enabled()) {
     // This case is not strictly necessary, but it makes the first-order only case
     // slightly more efficient.
-    Variable& grad_variable = as_variable_ref(grad);
-    if (grad_variable.is_sparse() && !new_grad.is_sparse()) {
+    if (variable_grad.is_sparse() && !new_grad.is_sparse()) {
       // If `grad_variable` is sparse and `new_grad` is not sparse, their sum is not
       // sparse, and we must change the TensorImpl type of `grad_variable` for it to
       // store the result. However, changing the TensorImpl type of a tensor requires
       // changing the tensor itself, and thus in this case we have to change the grad
       // tensor.
-      grad_variable = new_grad + grad_variable;
+      update_grad_fn(new_grad + variable_grad);
     } else {
       // In this case we can avoid changing the grad tensor. There are three scenarios
       // when we'll hit this case:
@@ -82,13 +75,34 @@ auto AccumulateGrad::apply(variable_list&& grads) -> variable_list {
       // In all of these three cases, `grad_variable += new_grad` is a valid operation
       // which adds `new_grad` to `grad_variable` in place. `grad_variable` is thus
       // still referring to the same tensor after the operation.
-      grad_variable += new_grad;
+      variable_grad += new_grad;
     }
   } else {
-    variable.grad() = grad + new_grad;
+    update_grad_fn(variable_grad + new_grad);
   }
+}
+
+auto AccumulateGrad::apply(variable_list&& grads) -> variable_list {
+  // XXX: this method is not thread-safe!
+  check_input_variables("AccumulateGrad", grads, 1, 0);
+
+  if (!grads[0].defined())
+    return {};
+  if (variable.grad_fn())
+    throw std::logic_error(
+        "leaf variable has been moved into the graph interior");
+  if (!variable.requires_grad())
+    return {};
+
+  auto new_grad = std::move(grads[0]);
+  at::Tensor& grad = variable.grad();
+  accumulateGradAndCallHooks(
+      variable,
+      grad,
+      new_grad,
+      !post_hooks().empty(),
+      [&grad](at::Tensor&& grad_update) { grad = grad_update; });
 
   return variable_list();
 }
-
 }} // namespace torch::autograd
